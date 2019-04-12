39980e81c4f1ae4542aeee41a212086def274dd9
Luke Dosen <lddosen@gmail.com>
2019-04-12 18:32:45 -0500

    prepartaion for hosting

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_bcrypt import Bcrypt
-# from flask_login import LoginManager
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
-"""
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://niuenxjulkpxav:09f3c861d5df0e5e06235f8a73fc54c2665864942b2459f9c96ff496d0cdcbb0@ec2-50-17-246-114.compute-1.amazonaws.com:5432/d688gbqp26k5fr'
+api = Api(app)
+
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -17,4 +17,4 @@ from flaskApp import routes
 from flaskApp import models
 
 models.db.create_all()
-"""
+
