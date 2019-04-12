a3ca2e9ca6311ac4add53817d7e2ac57fa277e82
Luke Dosen <lddosen@gmail.com>
2019-04-12 18:18:18 -0500

    preparations for server hosting

---
+++ Procfile.txt
@@ -0,0 +1,1 @@
+web: gunicorn flaskApp:app
--- flaskApp/routes.py
+++ flaskApp/routes.py
@@ -1,18 +1,19 @@
 from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask_httpauth import HTTPBasicAuth
 from flaskApp import app, db, bcrypt
-# import flaskApp.main_algorithm
+from flaskApp.main_algorithm import call_algorithm
+
+auth = HTTPBasicAuth
 
 
 @app.route("/", methods=['GET'])
 def index():
     return "<h1>hello, world</h1>"
 
-@app.route("/classdemo", methods=['GET', 'POST'])
+@app.route("/classdemo", methods=['GET'])
 def classDemo():
     if not request.json or not 'title' in request.json:
         abort(404)
-    sample_output_schedule = None # call the main algorithm here \
-    # TODO: refactor the main algorithm to be a class so args can be passed here
+    sample_output_schedule = call_algorithm()
 
     return jsonify(sample_output_schedule), 201
