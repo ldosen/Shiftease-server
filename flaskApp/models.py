22665f3ded61055364cc7780bb7213b05e4f7b7a
Luke Dosen <lddosen@gmail.com>
2019-04-14 14:11:42 -0500

    final db structure implemented in models

--- flaskApp/models.py
+++ flaskApp/models.py
@@ -7,9 +7,31 @@ from sqlalchemy import orm
 db.Model.metadata.reflect(db.engine)
 
 
-class Employee(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
     __table_args__ = {'extend_existing': True}
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    usertype = db.Column(db.String(10), nullable=False)
+
+class Employee(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    lname = db.Column(db.String(20), nullable=False)
+    fname = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"Employee('{self.id}', '{self.lname}', '{self.fname}')"
+
+
+class Manager(db.Model):
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     lname = db.Column(db.String(20), nullable=False)
     fname = db.Column(db.String(20), nullable=False)
 
@@ -28,6 +50,12 @@ class Shift(db.Model):
 
 
 class Available_For(db.Model):
+    __table_args__ = {'extend_existing': True}
+    employeeId = db.Column(db.Integer, db.ForeignKey('employee.id'), primary_key=True, nullable=False)
+    shiftId = db.Column(db.Integer, db.ForeignKey('shift.id'), primary_key=True, nullable=False)
+
+
+class Scheduled_For(db.Model):
     __table_args__ = {'extend_existing': True}
     employeeId = db.Column(db.Integer, db.ForeignKey('employee.id'), primary_key=True, nullable=False)
     shiftId = db.Column(db.Integer, db.ForeignKey('shift.id'), primary_key=True, nullable=False)
