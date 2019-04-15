b195327d7a49f0977dc9c94268003cc99c8d0f14
Luke Dosen <lddosen@gmail.com>
2019-04-14 21:50:20 -0500

    added cors request support

--- flaskApp/__init__.py
+++ flaskApp/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
@@ -7,6 +8,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://niuenxjulkpxav:09f3c861d5df0e5e06235f8a73fc54c2665864942b2459f9c96ff496d0cdcbb0@ec2-50-17-246-114.compute-1.amazonaws.com:5432/d688gbqp26k5fr'
 
+CORS(app)
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
