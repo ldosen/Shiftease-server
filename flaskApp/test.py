92327f0315443a3b5f838e55ff75fbc81c1511cb
Jonathan <warkentin.jon@gmail.com>
2019-04-24 01:11:52 -0500

    add tests that need to be filled in

--- flaskApp/test.py
+++ flaskApp/test.py
@@ -1,11 +1,38 @@
 import unittest
-from main_algorithm import call_algorithm
+import main_algorithm
 
 
 class TestMainAlgorithm(unittest.TestCase):
 
-    def test_algorithm(self):
-        self.assertNotEqual(call_algorithm(), None)
+    def setUp(self):
+        self.raw_availabilities = [0, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0]
+        self.slots_to_fill = {
+            2019: {4: {23: {"9am": None, "10am": None}, 24: {"9am": None, "10am": None}}}}
+        self.max_slots = 2
+        self.total_shifts = 4
+        self.employees = ["Luke", "George", "Zac"]
+        self.target_shifts = [2, 1, 1]
+        self.result = main_algorithm.make_schedule(
+            self.raw_availabilities, self.slots_to_fill, self.max_slots, self.total_shifts, self.employees, self.target_shifts)
+
+    def test_not_none(self):
+        self.assertNotEqual(self.result, None)
+
+    def test_db_queries(self):
+        # check db query equal to expected
+
+    def test_data_processed(self):
+        # check db queries properly transformed into necessary inputs for algorithm
+
+    def test_unschedulable(self):
+        # check if unschedulable list in result is equal to expected
+
+    def test_scheduled(self):
+        # check result slots_to_fill dict is equal to expected
+
+    def test_db_update(self):
+        # check if the db updates to reflect the output of the algorithm
+        # basically test_scheduled combined with test_db_queries
 
 
 if __name__ == '__main__':
